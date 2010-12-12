43bca9a069899ac5c4cf5b237f26e07d271bce44
Matthew J Morrison <mattj.morrison@gmail.com>
2010-12-12 12:50:25 -0600

    removed unneeded settings and django-admin site

--- src/urls.py
+++ src/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls.defaults import *
 
-from django.contrib import admin
-admin.autodiscover()
-
 from blog import views
 urlpatterns = patterns('',
-	(r'^admin/', include(admin.site.urls)),
 	(r'^$', views.foo),
 )
